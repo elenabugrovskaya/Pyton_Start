679c1d80e80493ff9c638c68c12114c40b3ecb11
Helen <helen@Mac.local>
2023-01-23 20:09:40 +0700

    XO

---
+++ candy/game.py
@@ -0,0 +1,9 @@
+import commands
+
+def start():
+    commands.set_player_names()
+    if commands.draw():
+        commands.switch_player()
+    while commands.get_total() > 0:
+        commands.game_turn()
+    print(f'{commands.get_current_player_name()} Забрал последние конфеты и победил!')
